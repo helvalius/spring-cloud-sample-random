1dc4d60fac7f2e5db3fc7173b42dbf758054cbaa
Jan Nonnen <jan.nonnen@gmx.net>
2015-12-08 20:21:54 +0100

    add hystrix with fallback

--- random-service/src/main/java/io/github/helvalius/fortune/teller/Application.java
+++ random-service/src/main/java/io/github/helvalius/fortune/teller/Application.java
@@ -3,11 +3,13 @@ package io.github.helvalius.fortune.teller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableEurekaClient
+@EnableHystrix
 public class Application {
 
     public static void main(String[] args) {
--- random-service/src/main/java/io/github/helvalius/fortune/teller/FortuneService.java
+++ random-service/src/main/java/io/github/helvalius/fortune/teller/FortuneService.java
@@ -1,5 +1,6 @@
 package io.github.helvalius.fortune.teller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +11,29 @@ import java.util.Random;
 @RestController
 public class FortuneService {
 
+    @HystrixCommand(fallbackMethod = "fixedNumber")
     @RequestMapping(path = "/seed", method= RequestMethod.GET)
     public int seed(){
         return new Random().nextInt();
     }
 
+    @HystrixCommand(fallbackMethod = "fixedSubstraction")
     @RequestMapping(path = "/subtractor", method=RequestMethod.POST)
     public int subtractor(@RequestParam(required = false) Integer minuend, @RequestParam(required = false) Integer subtrahend){
         int a = minuend != null ? minuend : new Random().nextInt();
         int b = subtrahend != null ? subtrahend : new Random().nextInt();
         return a-b;
     }
+
+    public int fixedNumber(){
+        return 42;
+    }
+
+    public int fixedSubstraction(){
+        return 11;
+    }
+
+
+
+
 }
